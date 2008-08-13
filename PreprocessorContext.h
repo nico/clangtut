e3d06a018c97e7dd26af0c850a7db8a4f407ce10
Nicolas Weber <nicolasweber@gmx.de>
2008-08-13 17:27:57 +0200

    .

---
+++ PreprocessorContext.h
@@ -0,0 +1,46 @@
+
+#include <string>
+
+#include "llvm/Config/config.h"
+
+#include "clang/Basic/Diagnostic.h"
+#include "clang/Basic/TargetInfo.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/Basic/FileManager.h"
+
+#include "clang/Lex/HeaderSearch.h"
+#include "clang/Lex/Preprocessor.h"
+
+#include "clang/Driver/TextDiagnosticPrinter.h"
+
+namespace clang {
+
+struct PPContext {
+  // Takes ownership of client.
+  PPContext(DiagnosticClient* client = 0,
+            const std::string& triple = LLVM_HOSTTRIPLE)
+    : diagClient(client == 0?new TextDiagnosticPrinter:client),
+      diags(diagClient),
+      target(TargetInfo::CreateTargetInfo(triple)),
+      headers(fm),
+      pp(diags, opts, *target, sm, headers)
+  {}
+
+  ~PPContext()
+  {
+    delete diagClient;
+    delete target;
+  }
+
+  DiagnosticClient* diagClient;
+  Diagnostic diags;
+  LangOptions opts;
+  TargetInfo* target;
+  SourceManager sm;
+  FileManager fm;
+  HeaderSearch headers;
+  Preprocessor pp;
+};
+
+}
+
