23244bf7aeae59069d13e8e1bf1870de48e509f9
Nicolas Weber <nicolasweber@gmx.de>
2008-08-26 21:02:50 +0200

    add blockquote style

--- linkify.py
+++ linkify.py
@@ -13,6 +13,7 @@ def linkify(line):
   newline = r.sub(r'[`\1`](\1)', line)
   return newline
 
+# all the cool kids use automatic tests these days
 assert linkify('bla') == 'bla'
 assert linkify('bla.h') == '[`bla.h`](bla.h)'
 assert linkify('bla.html') == 'bla.html'
@@ -20,12 +21,9 @@ assert linkify('bla.c') == '[`bla.c`](bla.c)'
 assert linkify('bla.cpp') == '[`bla.cpp`](bla.cpp)'
 assert linkify('`bla.cpp`') == '[`bla.cpp`](bla.cpp)'
 assert linkify('[`bla.cpp`]') == '[`bla.cpp`]'
-assert linkify('[bla.cpp]') == '[bla.cpp]'  # not sure about this one
+assert linkify('[bla.cpp]') == '[bla.cpp]'
 assert linkify('a.cpp and b.cpp') == '[`a.cpp`](a.cpp) and [`b.cpp`](b.cpp)'
 assert linkify('[tut05]: tut05_parse.cpp') == '[tut05]: tut05_parse.cpp'
-
-
-
 
 for line in fileinput.input():
    sys.stdout.write(linkify(line))
